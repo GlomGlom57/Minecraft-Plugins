4e33b82d7a92d2019b50d2cd376edd2d7860fa2a
GlomGlom57 <rodrigobm1318@gmail.com>
2025-11-03 16:23:48 -0300

    Fixing default message.

--- welcome_message/src/main/java/com/welcome_message/Main.java
+++ welcome_message/src/main/java/com/welcome_message/Main.java
@@ -4,23 +4,33 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.Listener;
+
 import org.bukkit.ChatColor;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener{
     @Override
     public void onEnable() {
-        getLogger().info("Welcome_Message iniciado!");
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info(ChatColor.DARK_PURPLE + "Welcome_Message iniciado!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Welcome_Message cerrado.");
+        getLogger().info(ChatColor.DARK_PURPLE + "Welcome_Message cerrado.");
     }
 
     @EventHandler
     public void onPlayerJoin (PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        player.sendMessage("Welcome, " + player.getName() + "!");
-        event.setJoinMessage(ChatColor.GREEN + player.getName() + " se ha conectado al servidor.");
+        player.sendMessage("Bienvenido, " + player.getName() + ".");
+
+        event.setJoinMessage(null);
+
+        final String connMesg = ChatColor.GREEN + player.getName() + " se ha conectado al servidor.";
+        for (Player p : player.getServer().getOnlinePlayers()) {
+            if (!p.equals(player))
+                p.sendMessage(connMesg);
+        }
     }
 }
